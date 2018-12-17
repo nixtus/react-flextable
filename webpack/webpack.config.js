eca53edf36a475a5b904fd66d03d438b7eaac661
Andrew N <aninneman@gmail.com>
2018-12-17 08:42:15 -0600

    webpack updates

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve('lib'),
     filename: 'index.js',
+    libraryTarget: 'commonjs2',
   },
   module: {
     rules: [
